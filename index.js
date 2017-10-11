f53b91de0644c969208d132dd9500b0989aeccad
Michael Wong <mik9080@gmail.com>
2017-10-11 18:02:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 function iterativeLog(array){
   array.forEach((element,index)=>{console.log(`${index}: ${element}`)})
 }
+
+function iterate(callback){
+  var array = ["one","two"]
+  array.forEach(callback)
+  return array
+}
+
+function doToArray(array,callback){
+  array.forEach(callback)
+}
